8bf802ef06814c428a5768e36528016083995d42
EC2 Default User <ec2-user@ip-172-31-44-144.us-east-2.compute.internal>
2020-05-08 03:25:52 +0000

    add new code

---
+++ src/hardesty/cicd.groovy
@@ -0,0 +1,15 @@
+#!/bin/groovy
+package hardesty
+
+import hudson.model.action
+import hudson.scm.ChangeLogSet
+import org.jenkinsci.plugins.workflow.graph.FlowNode
+import org.jenkinsci.plugins.workflow.cps.nodes.StepStartNode
+import org.jenkinsci.plugins.workflow.action.LabelActio
+import org.jenkinsci.apache.tools.ant.types.selectors.SelectorUtils
+
+import java.time.*
+
+def init() {
+  print "init ..."
+}
