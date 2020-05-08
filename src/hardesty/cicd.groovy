be3c5f6cb02584f74a8b6762d818c982c1c1a0fc
EC2 Default User <ec2-user@ip-172-31-44-144.us-east-2.compute.internal>
2020-05-08 03:40:03 +0000

    update

--- src/hardesty/cicd.groovy
+++ src/hardesty/cicd.groovy
@@ -6,7 +6,7 @@ import hudson.model.Action
 import hudson.scm.ChangeLogSet
 import org.jenkinsci.plugins.workflow.graph.FlowNode
 import org.jenkinsci.plugins.workflow.cps.nodes.StepStartNode
-import org.jenkinsci.plugins.workflow.action.LabelAction
+import org.jenkinsci.plugins.workflow.actions.LabelAction
 import org.apache.tools.ant.types.selectors.SelectorUtils
 
 import java.time.*
